cb7fbdbe865553ad76afdea5aef2e8fe12d582cb
Ishaq Ibrahim <ishaq.ibrahim@convertdigital.com.au>
2024-04-13 05:29:32 +0500

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use std::{io::{Read, Write}, net::TcpListener};
 
+use itertools::Itertools;
+
+fn extract_path(data: String) -> String {
+    let first_line = data.split("\r\n").next().unwrap();
+    let path_vec = first_line.split(" ").collect_vec();
+    let path = path_vec.get(1).unwrap();
+    String::from(*path)
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -10,10 +19,20 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                let mut buffer = [0; 10];
-                let incoming_data = _stream.read(&mut buffer);
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                let num_bytes_written = _stream.write(response.as_bytes());
+                let mut buffer = [0; 120];
+                let _num_incoming_bytes = _stream.read(&mut buffer);
+                let incoming_data = String::from_utf8(Vec::from(buffer)).unwrap();
+                
+                let path = extract_path(incoming_data);
+                
+                if path == "/" {
+                    let ok = "HTTP/1.1 200 OK\r\n\r\n";
+                    let _num_bytes_written = _stream.write(ok.as_bytes());
+                } else {
+                    let not_found = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+                    let _ = _stream.write(not_found.as_bytes());
+                }
+
             }
             Err(e) => {
                 println!("error: {}", e);
