80dc93b08d936240182754f00f62895e1acb64da
Ishaq Ibrahim <ishaq.ibrahim@convertdigital.com.au>
2024-04-13 05:46:10 +0500

    complete stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 use itertools::Itertools;
 
-fn extract_path(data: String) -> String {
+fn extract_random_string(data: String) -> String {
     let first_line = data.split("\r\n").next().unwrap();
     let path_vec = first_line.split(" ").collect_vec();
     let path = path_vec.get(1).unwrap();
-    String::from(*path)
+    let random_string = path.split("/echo/").last().unwrap();
+    String::from(random_string)
 }
 
 fn main() {
@@ -22,17 +26,12 @@ fn main() {
                 let mut buffer = [0; 120];
                 let _num_incoming_bytes = _stream.read(&mut buffer);
                 let incoming_data = String::from_utf8(Vec::from(buffer)).unwrap();
-                
-                let path = extract_path(incoming_data);
-                
-                if path == "/" {
-                    let ok = "HTTP/1.1 200 OK\r\n\r\n";
-                    let _num_bytes_written = _stream.write(ok.as_bytes());
-                } else {
-                    let not_found = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-                    let _ = _stream.write(not_found.as_bytes());
-                }
 
+                let random_string = extract_random_string(incoming_data);
+
+                let base_response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", random_string.len(), random_string);
+                println!("Sending: \n{}", base_response);
+                let _ = _stream.write(base_response.as_bytes());
             }
             Err(e) => {
                 println!("error: {}", e);
